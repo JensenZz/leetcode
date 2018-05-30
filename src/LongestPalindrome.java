a39f0fd4d84ede69cce7044f1d03222e235b300c
zhajx <zhajx@getui.com>
2018-05-30 14:55:36 +0800

    asd

--- src/LongestPalindrome.java
+++ src/LongestPalindrome.java
@@ -10,7 +10,45 @@
  */
 
 public class LongestPalindrome {
-    public String longestPalindrome(String s) {
-        return "";
+    public static String longestPalindrome(String s) {
+        int j = 1;
+        int i = 0;
+        String tempMax = "";
+        if (s.length() <= 1) {
+            tempMax = s;
+        } else {
+            tempMax = s.substring(0, 1);
+        }
+        while (i < s.length() && j < s.length()) {
+            //第一种情况  回环为偶数
+            if (s.charAt(i) == s.charAt(j)) {
+                int a = i, b = j;
+                while (a >= 0 && b < s.length() && s.charAt(a) == s.charAt(b)) {
+                    a--;
+                    b++;
+                }
+                if (b - 1 - a > tempMax.length()) {
+                    tempMax = s.substring(a + 1, b);
+                }
+            }
+            if (j + 1 < s.length() && s.charAt(i) == s.charAt(j + 1)) {
+                int a = i, b = j + 1;
+                while (a >= 0 && b < s.length() && s.charAt(a) == s.charAt(b)) {
+                    a--;
+                    b++;
+                }
+                if (b - 1 - a > tempMax.length()) {
+                    tempMax = s.substring(a + 1, b);
+                }
+            }
+            i++;
+            j++;
+
+        }
+        return tempMax;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(longestPalindrome(""));
     }
 }
